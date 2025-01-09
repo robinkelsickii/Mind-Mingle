0d6fb2d2f58b9f1abacfb8eca563c80a3301a178
robinkii <robinkelsickii@gmail.com>
2025-01-09 10:24:45 -0500

    Add Comment model with schema definition for author, content, and timestamps

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -6,4 +6,23 @@ const CommentSchema = new mongoose.Schema({
     ref: "Post",
     required: true,
   },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+module.exports = mongoose.model("Comment", CommentSchema);
