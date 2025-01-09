e5202d3c4b7c45aa4e159d885d1ed281f236aaa4
robinkii <robinkelsickii@gmail.com>
2025-01-09 10:49:01 -0500

    Refactor Message model to use timestamps and improve schema definition

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,30 +1,26 @@
 const mongoose = require("mongoose");
 
-const MessageSchema = new mongoose.Schema({
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+// Schema for storing messages between users
+const MessageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
   },
-  recipient: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  read: {
-    type: Boolean,
-    default: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Message", MessageSchema);
