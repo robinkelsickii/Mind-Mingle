c42d224404e941a561af8caf53308fa75e13a2f5
robinkii <robinkelsickii@gmail.com>
2025-01-09 10:48:09 -0500

    Refactor Like model to include timestamps and clean up schema definition

--- server/models/Like.js
+++ server/models/Like.js
@@ -1,37 +1,29 @@
 const mongoose = require("mongoose");
 
-const LikeSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+const LikeSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+    comment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    message: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    liked: {
+      type: Boolean,
+      default: false,
+    },
   },
-  post: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Post",
-    default: null,
-  },
-  comment: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Comment",
-    default: null,
-  },
-  message: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Message",
-    default: null,
-  },
-  liked: {
-    type: Boolean,
-    default: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Like", LikeSchema);
